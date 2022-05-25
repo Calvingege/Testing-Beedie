a5c585cd70bdc50f247e4a28230a59f931238e33
Spicy <bryan008@binus.ac.id>
2022-05-25 09:48:15 +0700

    about-us
    'Owen'

---
+++ resources/views/about-us.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body>
+<div id="navbar">
+        <ul>
+        <a href="home.blade.php"><li>Home</li></a>
+        <a href="login.blade.php"><li>login</li></a>
+        <a href="about_us.php"><li>about us</li></a>
+        </ul>
+      </div>
+
+    <div id="picture-selection">
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		<img src="../assets/online-auction-4.23.jpg" alt="ini gambar" style="width: 300px; height: 300px" class="floatRight">
+    </div>  
+
+    <div id="hero-section">
+            <h1>ABOUT US</h1>
+        <p align="justify">
+		ONLINE AUCTION adalah balai lelang yang melelang produk-produk otomotif seperti kendaraan roda dua dan roda empat, namun saat ini kami juga telah melelang barang-barang non-otomotif seperti gadget dan alat berat. ONLINE AUCTION berdiri sejak tahun 2007 yang merupakan perusahaan patungan dari investor luar dan dalam negeri.
+		<br><br>
+		Siapapun dapat berpartisipasi menjadi peserta lelang, baik perusahaan atau perorangan, dari pembelian skala besar hingga pembelian secara satuan.
+		<br><br>
+		ONLINE AUCTION mengadakan lelang secara rutin setiap minggunya di 10 cabang yang tersebar di Indonesia. Selain itu ONLINE AUCTION juga sudah mengadakan lelang di lebih dari 30 kota di Indonesia. Hingga akhir 2016, lebih dari 27.000 kendaraan roda empat telah terjual dan terdapat lebih dari 500 event lelang dapat diselenggarakan ONLINE AUCTION setiap tahunnya.
+		<br><br>
+		Anda tidak hanya dapat berpartisipasi di lelang untuk membeli kendaraan, namun ONLINE AUCTION juga bisa menjadi solusi penjualan untuk mitra perusahaan yang ingin menjual aset kendaraan mereka. Tidak hanya untuk perusahaan, sebagai perorangan Anda juga dapat menitipkan kendaraan untuk dijual melalui lelang ONLINE AUCTION.
+		</p>
+		
+    </div>
+	
+
+</body>
+</html>
